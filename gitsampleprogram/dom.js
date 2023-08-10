24e716d578ad42d0d1693e556e17d67133fdbc98
vininaik97 <vinayahri0912@gmail.com>
2023-08-11 00:15:30 +0530

    dom manipulation

---
+++ gitsampleprogram/dom.js
@@ -0,0 +1,21 @@
+//traversing the dom
+var item_list=document.querySelector('#items');
+//parent node
+console.log(item_list.parentNode);
+item_list.parentNode.style.backgroundColor=grey;
+console.log(item_list.parentNode.parentNode);
+
+//parentElement
+
+console.log(item_list.parentElement);
+item_list.parentElement.style.backgroundColor='grey';
+console.log(item_list..parentElement..parentElement);
+
+//childNode
+console.log(item_list.childNode);
+console.log(item_list.childNode[1]);
+item_list.item_list.childNode[1].style.backgroundColor='yellow';
+
+//firstcild
+console.log(item_list.firstChild);
+
